1b3d124a55298b0f856b1fa46782a1061e751ccb
mattialicausi <licausivitomattia@gmail.com>
2022-11-08 13:40:47 +0100

    aggiunto metodo(attivato da un bottone) per ciclare 10 volte l'API ed inserirlo in un array che verra visualizzato nel template con un v-for

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 'use strict';
 
+/*
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
 const {createApp} = Vue;
 
 //creo app
@@ -7,11 +16,19 @@ const {createApp} = Vue;
 const app = createApp({
     data(){
         return {
-            //dati
+            listaEmail: [],
         }
     },
     methods: {
-        //metodi
+        callApi(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email)=>{
+                    console.log(email.data.response);
+                    this.listaEmail.push(email.data.response);
+                })
+                
+            }
+        }
     },
     computed(){
         //computed
